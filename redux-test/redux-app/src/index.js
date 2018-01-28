4908d4365306bbaac1cfe007926b7141c51fc51a
jurin-n <jurin.nakano@gmail.com>
2018-01-28 20:39:08 +0900

    subscribeメソッドを試した。

--- redux-test/redux-app/src/index.js
+++ redux-test/redux-app/src/index.js
@@ -37,4 +37,52 @@ store.dispatch(
 console.log("after sending action")
 console.log(store.getState())
 
+
+console.log("# subscriber test ---------------")
+const logState = () => console.log('next state:',store.getState())
+
+console.log("subscribe logState.")
+const unsubscribeLogState = store.subscribe(logState)
+
+store.dispatch(
+    {
+        type:"ADD_ITEM",
+        id:"item101",
+        title:"商品１０１"
+    }
+)
+store.dispatch(
+    {
+        type:"ADD_ITEM",
+        id:"item102",
+        title:"商品１０２"
+    }
+)
+console.log("unsubscribe logState.")
+unsubscribeLogState()
+store.dispatch(
+    {
+        type:"ADD_ITEM",
+        id:"item103",
+        title:"商品１０３"
+    }
+)
+
+console.log("subscribe logStateStorage['redux-store'] example.")
+const store2 = createStore(
+    combineReducers({items, sort}),
+    (localStorage['redux-store'])?
+        JSON.parse(localStorage['redux-store']):
+        {}
+)
+store2.subscribe(()=>{
+    localStorage['redux-store'] = JSON.stringify(store2.getState())
+})
+store2.dispatch(
+    {
+        type:"ADD_ITEM",
+        id:"item201",
+        title:"商品２０１"
+    }
+)
 ReactDOM.render(<div>test</div>, document.getElementById('root'));
