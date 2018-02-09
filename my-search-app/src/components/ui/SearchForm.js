4a8d460c7a6d96c5aa1a51d066f41bbb7d0cf8c8
jurin-n <jurin.nakano@gmail.com>
2018-02-09 21:58:04 +0900

    検索窓に入れたキーワードを画面遷移しても保持する様に対応。

--- my-search-app/src/components/ui/SearchForm.js
+++ my-search-app/src/components/ui/SearchForm.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router'
 
-class SearchForm extends Component {
+class SearchForm extends Component {    
     constructor(props) {
         super(props)
         this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentWillMount(){
+        const {conditions} = this.props
+        this.setState({_keyword:this.defaultKeywordValue(conditions)})
     }
 
     handleSubmit(e){
@@ -19,10 +24,26 @@ class SearchForm extends Component {
         _keyword.focus()
     }
 
+    defaultKeywordValue(conditions){
+        const c = conditions.filter(c => c.key === "keyword")
+        
+        if(c.length === 0){
+            return ""
+        }
+
+        return c[0].value
+    }
+
     render(){
         return (
             <form className="form-inline" onSubmit={this.handleSubmit}>
-                <input ref="_keyword" className="form-control form-control-lg mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                <input ref="_keyword"
+                        className="form-control form-control-lg mr-sm-2" 
+                        type="search" 
+                        placeholder="Search"
+                        aria-label="Search"
+                        value={this.state._keyword}
+                        onChange={(e) => this.setState({_keyword: e.target.value})} />
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         )
