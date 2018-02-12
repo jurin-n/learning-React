a6b62e5f95a91801a3ea2811ed30bae918704ce8
jurin-n <jurin.nakano@gmail.com>
2018-02-12 16:09:19 +0900

    左側のナビゲーションに項目を２つ追加してみた。

--- my-search-app/src/components/ui/LeftNav.js
+++ my-search-app/src/components/ui/LeftNav.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 const LeftNav = () => 
     <div className="left-nav card col-md-3 order-first">
-        Second, but first<br/>
+        <div className="card">
+            <div className="card-header">
+                タグ
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">あとで読む</li>
+                <li className="list-group-item">AWS</li>
+            </ul>
+        </div>
+        <hr/>
+        <div className="card">
+            <div className="card-header">
+                登録日
+            </div>
+            <form>
+                <div className="form-row">
+                    <div className="col">
+                        <input type="date" className="form-control" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                    から
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                        <input type="date" className="form-control" />
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 
 export default LeftNav
